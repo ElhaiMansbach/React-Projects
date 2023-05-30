a793b0d1545b38f05594823f1ebe448453db843f
Elhai <elhaimn@gmail.com>
2023-05-30 18:37:30 +0300

    Added remove button

--- UserManager/src/components/UI/Button.js
+++ UserManager/src/components/UI/Button.js
@@ -1,15 +1,15 @@
-import React from "react";
+import React from 'react';
 
-import classes from "./Button.module.css";
+import classes from './Button.module.css';
 
 const Button = (props) => {
   return (
-    <button
+    <button style={{ marginRight: '10px'}}
       className={classes.button}
-      type={props.type || "button"}
+      type={props.type || 'button'}
       onClick={props.onClick}
     >
-        {props.children}
+      {props.children}
     </button>
   );
 };
