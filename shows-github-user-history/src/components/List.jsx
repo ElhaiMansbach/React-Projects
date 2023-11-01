944f015c2ab0452e8786a1c23aacde9b84bb2795
Elhai Mansbach <elhaimn@gmail.com>
2023-11-01 20:48:39 +0200

    Adding design

--- shows-github-user-history/src/components/List.jsx
+++ shows-github-user-history/src/components/List.jsx
@@ -2,19 +2,17 @@ import React from "react";
 import Item from "./Item";
 
 function List(props) {
+  const sortedRepos = [...props.userRepos].sort((repo1, repo2) => {
+    const date1 = new Date(repo1.created_at);
+    const date2 = new Date(repo2.created_at);
+    return date2 - date1;
+  });
   return (
-    <>
-      <div>This user has {props.userRepos.length} public repositories</div>
-      <ul>
-        {props.userRepos.map((repo, index) => {
-          return (
-            <li key={index}>
-              <Item key={index} repo={repo} />
-            </li>
-          );
-        })}
-      </ul>
-    </>
+    <div style={{ marginTop: "80px" }}>
+      {sortedRepos.map((repo, index) => {
+        return <Item key={index} repo={repo} />;
+      })}
+    </div>
   );
 }
 
